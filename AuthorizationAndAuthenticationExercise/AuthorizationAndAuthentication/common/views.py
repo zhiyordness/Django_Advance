3f1938eecd938627ba957d5c99206e0eae48911a
Zhivomir Yordanov <zhivomiryordanov@gmail.com>
2026-03-09 19:14:26 +0200

    all

--- AuthorizationAndAuthenticationExercise/AuthorizationAndAuthentication/common/views.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
-from django.views.generic import TemplateView
-
-
-class HomeView(LoginRequiredMixin, TemplateView):
-    template_name = 'home.html'
